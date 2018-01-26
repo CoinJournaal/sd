219e9068b155fff24f5f85e6d430d8413a274c57
CoinJournaal <34217520+CoinJournaal@users.noreply.github.com>
2018-01-26 15:14:53 +0100

    Update server.js

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var express = require("express"),
     app = express();
 var fs = require("fs");
 var Canvas = require("canvas");
+var GIFEncoder = require('gifencoder');
 
 var port = process.env.PORT || 5000;
 
@@ -18,6 +19,12 @@ app.get("/png", function (request, res) {
 });
 
 function draw() {
+    var encoder = new GIFEncoder(320, 240);
+    encoder.start();
+    encoder.setRepeat(0);   // 0 for repeat, -1 for no-repeat
+    encoder.setDelay(500);  // frame delay in ms
+    encoder.setQuality(10); // image quality. 10 is default.
+    
     var Canvas = require('canvas'),
         Image = Canvas.Image,
         canvas = new Canvas(200, 200),
@@ -27,8 +34,21 @@ function draw() {
     ctx.rotate(0.1);
     ctx.fillText('Awesome!', 50, 100);
     
-    var buf = canvas.toBuffer();
-    fs.writeFileSync("public/test.png", buf);
+    // first frame
+    encoder.addFrame(ctx);
+    
+    // green rectangle
+    ctx.fillStyle = '#00ff00';
+    ctx.fillRect(0, 0, 200, 200);
+    encoder.addFrame(ctx);
+    
+    encoder.finish();
+    
+    var buf = encoder.out.getData();
+    fs.writeFile('public/test.gif', buf, function (err) {
+      // animated GIF written to myanimated.gif
+        console.log("write error: " + err);
+    });
 
     return canvas;
 }
