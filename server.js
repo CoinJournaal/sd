14a53c51bd5a6a81d09b2200a7229d27c347da9c
CoinJournaal <34217520+CoinJournaal@users.noreply.github.com>
2018-02-02 16:40:16 +0100

    Update server.js

--- server.js
+++ server.js
@@ -44,18 +44,18 @@ function draw(img,outputData) {
 		ctx.font = 'bold 30px Arial';
 		if(i<5) { 
 			ctx.fillStyle = '#00ff00';
-			ctx.fillText('#'+(i+1)+' Stijger', (40-j*10), 70);
+			ctx.fillText('#'+(i+1)+' Stijger', (40-(5-j)*10), 70);
 		}
 		else {
 			ctx.fillStyle = '#ff0000';
-			ctx.fillText('#'+(i-4)+' Daler', (40-j*10), 70);
+			ctx.fillText('#'+(i-4)+' Daler', (40-(5-j)*10), 70);
 		}
 		ctx.fillStyle = '#ffffff';
 		ctx.font = 'bold 50px Arial';
-		ctx.fillText(decodeURI(outputData[i][0]), (40-j*10), 130);
+		ctx.fillText(decodeURI(outputData[i][0]), (40-(5-j)*10), 130);
 		ctx.font = '30px Arial';
-		ctx.fillText(outputData[i][1], (40-j*10), 175);
-		ctx.fillText(outputData[i][3] + " USD", (200-j*50), 175);
+		ctx.fillText(outputData[i][1], (40-(5-j)*10), 175);
+		ctx.fillText(outputData[i][3] + " USD", (200-(5-j)*50), 175);
 		if(i<5) ctx.fillStyle = '#00ff00';
 		else ctx.fillStyle = '#ff0000';
 		ctx.font = 'bold 50px Arial';
